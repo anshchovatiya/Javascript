a633cdad48cffb284f919557d115019792b04a49
anshchovatiya <sqanshchovatiya800@gmail.com>
2024-06-20 07:54:51 +0530

    gredient generator completed

--- Practice/KeyboardEvents/script.js
+++ Practice/KeyboardEvents/script.js
@@ -8,13 +8,14 @@ let eventType = document.getElementById("eventType");
 
 
 
-
+// it activate when you press key
 textInput.addEventListener("keydown",(event)=>{
   keyPressed.innerText = `Key Pressed : ${event.key}`;
   asciiValue.innerText = `ASCII value : ${event.keyCode}`;
   eventType.innerText = `Event Types : ${event.type}`;
 });
 
+// it activates when you release pressed  key
 // textInput.addEventListener("keyup",(event)=>{
 //   keyPressed.innerText = `Key Pressed : ${event.key}`;
 //   asciiValue.innerText = `ASCII value : ${event.keyCode}`;
@@ -29,21 +30,25 @@ let Input = document.getElementById("Input");
 let InputLength = document.getElementById("inputLength");
 let eventType2 = document.getElementById("eventType2");
 
-let length =0;
 
-textInput2.addEventListener("input", (event) => {
-  Input.innerText = `Input : ${event.target.value}`;
-  length++;
-  keyPressed2.innerText = `Key Pressed : ${event.target.name}`;
-  inputType.innerText = `input type : ${event.target.type}`;
-  InputLength.innerText = `Event Types : ${length}`;
-
-});
-// textInput2.addEventListener("hashchange", (event) => {
+// it works when you enter input to the input field 
+// textInput2.addEventListener("input", (event) => {
+//   let InputLen = event.target.value;
 //   Input.innerText = `Input : ${event.target.value}`;
-//   length--;
 //   keyPressed2.innerText = `Key Pressed : ${event.target.name}`;
-//   inputType.innerText = `input type : ${event.target.type}`;
-//   InputLength.innerText = `Event Types : ${length}`;
+//   inputType.innerText = `input type : ${event.inputType}`;
+//   InputLength.innerText = `Event Types : ${InputLen.length}`;
 
 // });
+
+
+// it only called when you press enter or click outside input box
+textInput2.addEventListener("change", (event) => {
+  let InputLen = event.target.value;
+  Input.innerText = `Input : ${event.target.value}`;
+  keyPressed2.innerText = `Key Pressed : ${event.target.name}`;
+  inputType.innerText = `input type : ${event.inputType}`;
+  InputLength.innerText = `Event Types : ${InputLen.length}`;
+
+});
+
