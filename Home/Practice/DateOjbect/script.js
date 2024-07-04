86b65932db3bbce38312346768dc654fbe47761c
anshchovatiya <sqanshchovatiya800@gmail.com>
2024-07-04 23:26:58 +0530

    Date object complted

---
+++ Home/Practice/DateOjbect/script.js
@@ -0,0 +1,19 @@
+// function addDay(currentDate,AdditionalDays){
+//     let currentDay = currentDate.getDate() + AdditionalDays;
+//     currentDate.setDate(currentDay);
+//     return currentDate;
+
+// }
+
+
+// let date = new Date("07-4-2024");
+
+// let newDate =(addDay(date,10));
+// console.log(newDate);
+
+function print(v){
+    console.log("hello",v);
+}
+
+let v = 8;
+setInterval(()=>print(v),3000);0
