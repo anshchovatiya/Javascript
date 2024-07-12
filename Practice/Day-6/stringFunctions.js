680c86e4c3b923170060080bca534e31aa1eca87
Dhaval Solanki <dhavalas24@gmail.com>
2024-07-12 09:56:37 +0530

    string function practice program added

--- Practice/Day-6/stringFunctions.js
+++ Practice/Day-6/stringFunctions.js
@@ -1,3 +1,4 @@
+
 // 1. Write a JS function that reverse a number. 
 // Example x = 15438;
 // Expected Output : 83451
@@ -15,7 +16,7 @@ const reverseNumber = (number) => {
     return ans;
 };
 let number = reverseNumber(3432);
-console.log(number);
+// console.log(number);
 
 
 
@@ -26,13 +27,56 @@ console.log(number);
 // Assume punctuation and numbers symbols are not included in the passed string.
 
 
+function sortString (string){
+    let stringArray = string.split("");
+    stringArray.sort();
 
+    // filter method will remove all the blank spaces from the string
+    let ans = stringArray.filter((character)=>{
+        return character != " ";
+    })
+    let str = "";
+    // this loop will cocate the string
+    for(let current of ans){
+        str += current;
+    }
+    return str;
+}
+
+// console.log(sortString("chovatiya Ansh"));
 
 
 
 // 3. Write a JS function that accepts a string as a parameter and converts the first letter of each word of the string in upper case. 
 // Example string : 'the quick brown fox'
 // Expected Output : 'The Quick Brown Fox'
+
+function convertLetterUpperCase(str){
+
+    let array = str.split("");
+
+    array[0] = array[0].toUpperCase();
+
+    for(let i = 0; i<array.length; i++){
+        if(array[i]===" "){
+            i++;
+            array[i] = array[i].toUpperCase();
+        }
+    }
+   
+    let ans = "";
+
+    for(let current of array){
+        ans += current;
+    }
+    return ans;
+    
+}
+
+console.log(convertLetterUpperCase("my name is ansh"));
+
+
+
 
 // 4. Write a JavaScript function that accepts a string as a parameter and finds the longest word within the string.
 // Example string : 'Web Development Tutorial'
