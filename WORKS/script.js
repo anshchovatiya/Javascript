788ec4b83cbc028461eb77b255b2e5cf408a3da7
anshchovatiya <sqanshchovatiya800@gmail.com>
2024-07-23 14:35:22 +0530

    works project homepage added

--- WORKS/script.js
+++ WORKS/script.js
@@ -1,12 +1,27 @@
-import LocomotiveScroll from './node_modules/locomotive-scroll';
+// import LocomotiveScroll from './node_modules/locomotive-scroll';
+
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector("main"),
+//     smooth: true,
+// });
+
+const navBarOptions = document.querySelector(".nav-options");
+const navPlus = document.querySelector(".nav-plus")
 
 
-
-// LocomotiveScroll is constructor
-const locomotiveScroll = new LocomotiveScroll({
-    el : document.querySelector("main"),
-    smooth : true,
-});
+document.querySelector(".nav-plus").addEventListener("click",()=>{
 
 
-console.log("scroll is working");
+    if(navPlus.getAttribute("data-active")==="true"){
+        
+    }
+
+    gsap.to(".nav-plus",{
+        rotate : 45,
+    })
+
+    gsap.to(".nav-options",{
+        right : 0,
+    })
+    
+})
