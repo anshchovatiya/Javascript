9c89aa6b7670fea810524210a149afed3754472c
anshchovatiya <sqanshchovatiya800@gmail.com>
2024-07-25 05:41:44 +0530

    slider Added

---
+++ Slider/script.js
@@ -0,0 +1,36 @@
+let imgs = document.querySelectorAll(".img");
+
+let counter = 0;
+
+
+let positionSetting = () => {
+    imgs.forEach((current,index)=>{
+        current.style.left = `${index*100}%`
+    })
+}
+
+positionSetting();
+
+
+
+
+let Slider  = () => {
+    imgs.forEach((current)=>{
+          current.style.transform = `translateX(-${counter * 100}%)`
+    })
+}
+
+
+function goLeft(){
+    if(counter>=0){
+
+        counter--;
+        Slider();
+    }
+}
+function goRight(){
+    if(counter<3){
+        counter++;
+        Slider();
+    }
+}
