cd05a7e98f1f24352db43d06d6c532fc49ccc731
anshchovatiya <sqanshchovatiya800@gmail.com>
2024-08-02 15:57:39 +0530

    ajax Program added

---
+++ Practice/jQuery/Ajex.js
@@ -0,0 +1,33 @@
+
+$(
+
+    $("#ReloadButton").click(() => {
+        // $("#Text1").load("Ajex.text",(response,status,xhr)=>{
+        //     console.log(response);
+        //     console.log(status);
+        //     console.log(xhr);
+        // });
+        // $("#Text2").load("Ajex.text #para");
+
+        // $.get("Ajex.text",(result,status)=>{
+        //     console.log(result);
+        //     console.log(status);
+        // })
+
+        //    { // $.post("Ajex.text","this data is send by javascript file",(response,status)=>{
+        //     console.log(response);
+        //     console.log(status);
+        // })
+
+
+        //     $.post("demo_test_post.asp",
+        //     {
+        //       name: "Donald Duck",
+        //       city: "Duckburg"
+        //     },
+        //     function(data, status){
+        //       alert("Data: " + data + "\nStatus: " + status);
+        //     });
+        //    }
+    })
+);
